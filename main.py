ebd0612241d11afe749ef8da26d3aece82105539
李鑫 <1447893984@qq.com>
2020-09-08 12:47:22 +0800

    git使用测试1

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ from tqdm import tqdm
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 from math import log
+
+rawdata_filepath = './PacificExcellent_CCS/PacificExcellent_CCS'
+rawdata_filelist = os.listdir(rawdata_filepath)
